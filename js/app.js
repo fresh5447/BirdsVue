b3c31d6982b35b4373735ed5f54e725d68c87e10
fresh5447 <doug@kosmojo.com>
2017-03-01 11:03:09 -0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,55 +1,25 @@
+var title = "Birds Collection";
+
+function Bird(name, species, gender, img) {
+  this.name = name;
+  this.species = species;
+  this.gender = gender;
+  this.img = img;
+}
+
+var b1 = new Bird('Donald', 'Malard', 'male', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Anas_platyrhynchos_male_female_quadrat.jpg/440px-Anas_platyrhynchos_male_female_quadrat.jpg')
+var b2 = new Bird('Huey', 'Blue Jay', 'female', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSQSuV-sKmH9NtmTdYdGUZm4yhEgekNaE0R6s8um7KR-trqxl4D');
+var b3 = new Bird('Cornelius', 'Owl', 'male', 'https://download.ams.birds.cornell.edu/api/v1/asset/28488511/large');
+var b4 = new Bird('Reginold', 'Parrot', 'male', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQFT9pUBAkid56nIOntHsX3SFvYnlh7u44ft-BC_Z4Kb19JfHjoQg');
+var b5 = new Bird('Tiffany', 'Bald Eagle', 'female', 'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/A-G/bald-eagle-closeup.adapt.352.1.jpg');
+var b6 = new Bird('Craig', 'Cardinal', 'male', 'https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2016/highwaynoise.jpg');
+
+var birds = [b1, b2, b3, b4, b5, b6];
+
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello from app js!'
+  'el': '#app',
+  'data': {
+    title: title,
+    birds: birds
   }
-});
-
-var ts = [
-  { text: 'Learn JavaScript' },
-  { text: 'Learn Vue' },
-  { text: 'Build something awesome' }
-];
-
-var app2 = new Vue({
-  el: '#app2',
-  data: {
-    todos: ts,
-    numbers: [1, 4, 5, 7, 8, 10]
-  }
-});
-
-var fishies = [
-  { type: "halibut", ocean: "pacific" },
-  { type: "atlantic salmon", ocean: "atlantic" },
-  { type: "blue tuna", ocean: "indian" },
-  { type: "puffer fish", ocean: "caribbean" },
-  { type: "clown fish", ocean: "caribbean" },
-  { type: "squid", ocean: "pacific" },
-  { type: "king fish", ocean: "atlantic" },
-  { type: "blue tang", ocean: "pacific" },
-  { type: "red fish", ocean: "indian" },
-  { type: "blue fish", ocean: "atlantic" },
-  { type: "one fish", ocean: "arctic" },
-];
-
-var pacificFish = [];
-for (var i = 0; i < fishies.length; i++) {
-  if(fishies[i].ocean === "pacific"){
-    pacificFish.push(fishies[i]);
-  }
-};
-
-var caribbeanFish = fishies.filter(function( fish ){
-  return fish.ocean === "caribbean";
-});
-
-
-var app3 = new Vue({
-  el: '#app3',
-  data: {
-    fishies: fishies,
-    pacificFish: pacificFish,
-    caribbeanFish: caribbeanFish,
-  }
-});
+})
